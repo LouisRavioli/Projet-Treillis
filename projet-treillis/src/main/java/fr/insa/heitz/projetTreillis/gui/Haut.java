79b96e12338a4972607ce62fdc7420644209aeac
LouisRavioli <louis.heitz75@gmail.com>
2022-04-29 01:31:31 +0200

    eho

--- projet-treillis/src/main/java/fr/insa/heitz/projetTreillis/gui/Haut.java
+++ projet-treillis/src/main/java/fr/insa/heitz/projetTreillis/gui/Haut.java
@@ -2,35 +2,65 @@ package fr.insa.heitz.projetTreillis.gui;
 
 import java.util.ArrayList;
 
+import javafx.geometry.Orientation;
 import javafx.scene.control.Button;
-import javafx.scene.control.MenuButton;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.Separator;
+import javafx.scene.control.SeparatorMenuItem;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.control.Tooltip;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class Haut extends VBox {
 
-	private HBox l1;
-	private BorderPane l2;
-	private HBox l2Gauche;
-	private HBox l2Droite;
+	private MenuBar mbL1;
+	private BorderPane bpL2;
+	private HBox hbL2Gauche;
+	private HBox hbL2Droite;
 	
-	private ArrayList<MenuButton> menuButtonsL1;
+	private ArrayList<Menu> menusL1;
 	private ArrayList<Button> buttonsL2;
 	private ArrayList<ToggleButton> toggleButtonsL2;
 	
 	public Haut() {
-		menuButtonsL1 = new ArrayList<MenuButton>();
-		String[] noms = {"Fichier", "Edition", "Affichage"};
-		for (int i = 0; i <= 2; i++) {
-			MenuButton current = new MenuButton(noms[i]);
-			current.getStyleClass().addAll("haut-l1-menu-button", "haut-l1-menu-button" + (i + 1));
-			menuButtonsL1.add(current);
+		mbL1 = new MenuBar();
+		mbL1.getStyleClass().add("haut-l1-menu-bar");
+		String[] nomsMenus = {"Fichier", "Edition", "Affichage"};
+		String[][] nomsMenuItems = {{"Nouveau", "Ouvrir", "Enregistrer"}, {"Annuler", "Répéter", "Couper", "Copier", "Coller"}, {"Zoom avant", "Zoom arrière", "Taille réelle", "Ajuster à la fenêtre"}};
+		int[] indicesSeparateursL1 = {2, 2, -1};
+		for (int i = 0; i < nomsMenus.length; i++) {
+			Menu currentMenu = new Menu(nomsMenus[i]);
+			for (int j = 0; j < nomsMenuItems[i].length; j++) {
+				currentMenu.getItems().add(new MenuItem(nomsMenuItems[i][j]));
+			}
+			if (indicesSeparateursL1[i] != -1) {
+				currentMenu.getItems().add(indicesSeparateursL1[i], new SeparatorMenuItem());
+			}
+			mbL1.getMenus().add(currentMenu);
 		}
-		l1 = new HBox();
-		l1.getChildren().addAll(menuButtonsL1);
 		
-		getChildren().addAll(l1);
+		hbL2Gauche = new HBox();
+		hbL2Gauche.getStyleClass().add("haut-l2-hbox");
+		String[] toolTipsButtons = {"Nouveau", "Ouvrir", "Enregistrer", "Couper", "Copier", "Coller", "Annuler", "Répéter"};
+		int[] indicesSeparateursL2 = {3, 7};
+		for (int i = 0; i < toolTipsButtons.length; i++) {
+			Button currentButton = new Button();
+			currentButton.setTooltip(new Tooltip(toolTipsButtons[i]));
+			currentButton.getStyleClass().addAll("haut-l2-button", "haut-l2-button" + (i + 1));
+			hbL2Gauche.getChildren().add(currentButton);
+		}
+		for (int i : indicesSeparateursL2) {
+			Separator currentSeparator = new Separator();
+			currentSeparator.getStyleClass().add("haut-l2-separator");
+			hbL2Gauche.getChildren().add(i, currentSeparator);
+		}
+		bpL2 = new BorderPane(null, null, null, null, hbL2Gauche);
+		bpL2.getStyleClass().add("haut-l2-border-pane");
+		
+		getChildren().addAll(mbL1, bpL2);
 	}
 }
